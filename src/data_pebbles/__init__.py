1ba5c7bddfce93e0b363f7559cf56b8bfd6d29da
Leon David Zipp <leondavidzipp@gmx.de>
2026-03-14 23:39:00 +0100

    feat: reexporting mlflow here

--- src/data_pebbles/__init__.py
+++ src/data_pebbles/__init__.py
@@ -1,3 +1,10 @@
-from data_pebbles.sdk import BronzeLayer, DataPebbles, GoldLayer, SilverLayer
+import mlflow
 
-__all__ = ["DataPebbles", "BronzeLayer", "SilverLayer", "GoldLayer"]
+from data_pebbles.sdk import (
+	BronzeLayer,
+	DataPebbles,
+	GoldLayer,
+	SilverLayer,
+)
+
+__all__ = ["DataPebbles", "BronzeLayer", "SilverLayer", "GoldLayer", "mlflow"]
